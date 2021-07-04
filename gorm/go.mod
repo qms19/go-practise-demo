7a72913bb1df6965eda5c4780367486e2031efba
xiaoqing <501299076@qq.com>
2021-07-04 17:20:50 +0800

    feat: add gorm

---
+++ gorm/go.mod
@@ -0,0 +1,10 @@
+module github.com/qms19/go-practise-demo/gorm
+
+go 1.14
+
+require (
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	gorm.io/driver/mysql v1.1.1
+	gorm.io/gorm v1.21.11
+)
--- gorm/main.go
+++ gorm/main.go
@@ -1,5 +1,73 @@
 package main
+import (
+	"github.com/google/uuid"
+	"github.com/spf13/pflag"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"fmt"
+)
+
+type User struct {
+	gorm.Model
+	Username string `json:"username" gorm:"size:64"`
+	Password string `json:"password" gorm:"size:64"`
+	UUID string `json:"uuid" gorm:"column:uuid"`
+	Email string `json:"email" gorm:"size:32"`
+}
+
+func (u *User) TableName() string {
+	return "user"
+}
+
+func (u *User) AfterCreate(tx *gorm.DB) (err error) {
+	u.UUID = uuid.NewString()
+	return tx.Save(u).Error
+}
+
+
+var (
+	host = pflag.StringP("host","H","127.0.0.1","mysql server addr")
+	username = pflag.StringP("username","u","root","mysql username")
+	password = pflag.StringP("password","p","root","mysql password")
+	database = pflag.StringP("database","d","gtest","mysql database")
+	help = pflag.BoolP("help","h",false,"help message")
+)
 
 func main()  {
+
+	pflag.CommandLine.SortFlags = false
+	pflag.Usage = func() {
+		pflag.PrintDefaults()
+	}
+	pflag.Parse()
+	if *help {
+		pflag.Usage()
+		return
+	}
+
+	//dsn := "root:root@tcp(127.0.0.1:3306)/gtest?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf(`%s:%s@tcp(%s)/%s?charset=utf8&parseTime=%t&loc=%s`,
+		*username,
+		*password,
+		*host,
+		*database,
+		true,
+		"Local")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.AutoMigrate(User{})
+	PrintList(db)
+}
+
+func PrintList(db *gorm.DB)  {
+	users := []*User{}
+	db.Find(&users)
+
+	for _, user := range  users {
+		fmt.Printf("用户:%s\n",user.Username)
+	}
 
 }
